f000f184c76d281d22149d4cff0208bd9d385dcb
panduagam <pandupanatagama123@gmail.com>
2024-03-19 09:22:59 +0700

    [ADD] pesan pembuka

--- TugasPraktikumPemlan03/LeagueOfFighters/HeroMain.java
+++ TugasPraktikumPemlan03/LeagueOfFighters/HeroMain.java
@@ -1,7 +1,19 @@
 package LeagueOfFighters;
+ 
+import java.util.Scanner;
 
 public class HeroMain {
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        
+        System.out.println("Welcome to the League of Fighters!");
+
+        System.out.print("Anda akan diminta untuk memilih dua nama Champion yang akan bertarung.\n"
+                        + "Berikan juga nilai HP (Health Point), Attack, dan Defense untuk kedua Champion.\n"
+                        + "Untuk bagian angka, Anda hanya bisa memasukkan angka antara 100 hingga 1000.\n"
+                        + "===================================================================================\n");
+        
+        
         
     }
 }
